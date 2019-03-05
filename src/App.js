bfed9e0f1c9b2a8ad6f7ae312f63005a344aef4a
Mohammed <m7amd.s3ad@hotmail.com>
2019-03-05 14:10:56 +0300

    Fatima & Mohammed J --> after changing the content dynamiclly

--- src/App.js
+++ src/App.js
@@ -21,45 +21,21 @@ class App extends Component {
         <div className="authors">
           <h3>Authors</h3>
           <div className="row">
-            <div className="col-lg-4 col-md-6 col-12">
+     {authors.map(elem =>  
+            <div className="col-lg-4 col-md-6 col-12">           
               <div className="card">
                 <div className="image">
-                  <img className="card-img-top img-fluid" src={authors[0].imageUrl} alt="J.K. Rowling"/>
+                  <img className="card-img-top img-fluid" src={elem.imageUrl} alt="J.K. Rowling"/>
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">
-                    <span>{authors[0].first_name} {authors[0].last_name}</span>
+                    <span>{elem.first_name} {elem.last_name}</span>
                   </h5>
-                  <small className="card-text">{authors[0].books.length} books</small>
+                  <small className="card-text">{elem.books.length} books</small>
                 </div>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6 col-12">
-              <div className="card">
-                <div className="image">
-                  <img className="card-img-top img-fluid" src={authors[1].imageUrl} alt="George R.R. Martin"/>
-                </div>
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <span>{authors[1].first_name} {authors[1].last_name}</span>
-                  </h5>
-                  <small className="card-text">{authors[1].books.length} books</small>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 col-12">
-              <div className="card">
-                <div className="image">
-                  <img className="card-img-top img-fluid" src={authors[2].imageUrl} alt="J.R.R Tolkein"/>
-                </div>
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <span>{authors[2].first_name} {authors[2].last_name}</span>
-                  </h5>
-                  <small className="card-text">{authors[2].books.length} books</small>
-                </div>
-              </div>
-            </div>
+        )}
           </div>
         </div>
       </div>
